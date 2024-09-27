dbbfdafc2a8374a43c7f2a61ddc88f0beba5f9be
Marius Jensen <marius@clorith.net>
2024-09-27 19:51:01 +0200

    PHPStan: `is_countable` is always true when inside a countable loop for that variable

--- mu-plugins/troubleshooting-mode.php
+++ mu-plugins/troubleshooting-mode.php
@@ -1576,11 +1576,11 @@ class MustUse {
 													_n(
 														'Show %d remaining theme',
 														'Show %d remaining themes',
-														( is_countable( $themes ) ? ( count( $themes ) - 5 ) : 0 ),
+														( count( $themes ) - 5 ),
 														'troubleshooting'
 													)
 												),
-												\esc_html( (string) ( is_countable( $themes ) ? ( count( $themes ) - 5 ) : 0 ) )
+												\esc_html( (string) ( count( $themes ) - 5 ) )
 											)
 										);
 									}
